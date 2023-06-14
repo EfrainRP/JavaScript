62fb158efcf7627309ac64b79665dd89ffa9a1e8
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2023-06-13 21:35:12 -0600

    Numbers

---
+++ 4-Numbers/numbers.js
@@ -0,0 +1,22 @@
+/*let a =0.1;
+console.log(a);
+
+let b =0.2;
+
+let c= a+b;
+console.log(c);
+
+console.log(Math.round((0.1 + 0.2)*100)/100)
+
+let a =  3.5;
+let b = 4.8;
+console.log(a-b)*/
+let alturaCM = 185;
+let alturaM = 1.85;
+let pesoKG = 80.9;
+
+let F_alturaM = alturaM.toFixed(1);
+console.log(F_alturaM)
+
+let P_alturaM = parseFloat()
+console.log(P_alturaM)
